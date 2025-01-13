ec8e1ef3d7609b65703952b00187eb1cd5d63a36
Sean Ho@TheGigabit <sean.ho@thegigabit.com>
2025-01-13 17:25:19 +0800

    refactor: Update Flask environment settings and refactor blueprint URL prefixes

--- app/__init__.py
+++ app/__init__.py
@@ -2,15 +2,16 @@ class RegisterAppResources:
     @staticmethod
     def register_resources(app):
         print("In the register_resources method")
+        URL_PREFIX = "/api/app"
         # fmt:off
         from app.applications.urls import blp as app_url
-        app.register_blueprint(app_url, url_prefix="/app")
+        app.register_blueprint(app_url, url_prefix=URL_PREFIX)
 
         from app.applications.upstreams.urls import blp as upstream_url
-        app.register_blueprint(upstream_url, url_prefix="/app")
+        app.register_blueprint(upstream_url, url_prefix=URL_PREFIX)
 
         from app.applications.rules.urls import blp as rule_url
-        app.register_blueprint(rule_url, url_prefix="/app")
+        app.register_blueprint(rule_url, url_prefix=URL_PREFIX)
         # fmt:on
 
         return app
